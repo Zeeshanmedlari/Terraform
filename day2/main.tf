e7b6bfee9ff6c349f8ee3cc5545f57274a9d255d
Zeeshan <zeeshanmedlari@gmail.com>
2024-02-07 12:31:23 +0530

    Adding providers and variables

--- day2/main.tf
+++ day2/main.tf
@@ -1,28 +1,3 @@
-variable "AMI_value" {
-    description = "ami id"
-}
-
-variable "instance_type_value" {
-    description = "Value for instance_type"
-    default = "string"  
-}
-
-variable "subnet_value" {
-    description = "value for Subnet"
-}
-
-variable "key_name_value" {
-    description = "value for key_name"
-}
-
-
-
-
-
-provider "aws" {
-    region = "us-west-1"
-}
-
 resource "aws_instance" "Learning_instance" {
     ami = var.AMI_value
     instance_type = var.instance_type_value
---
+++ day2/variables.tf
@@ -0,0 +1,16 @@
+variable "AMI_value" {
+    description = "ami id"
+}
+
+variable "instance_type_value" {
+    description = "Value for instance_type"
+    default = "string"  
+}
+
+variable "subnet_value" {
+    description = "value for Subnet"
+}
+
+variable "key_name_value" {
+    description = "value for key_name"
+}
